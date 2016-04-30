a28e90fdf5f15e1111434bed5a016193d9aea1f7
Tim Gillies <timothy.gillies@colorado.edu>
2016-04-29 22:36:11 -0600

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'bcrypt', '3.1.7'
+gem 'faker', '1.4.2'
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns a user in a persistent session.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the current logged-in user (if any)
   def current_user
     if (user_id = session[:user_id])
@@ -40,5 +45,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
