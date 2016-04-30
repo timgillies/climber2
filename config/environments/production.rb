aefd831e17955232597f3b14fb296d54613554ff
Tim Gillies <timothy.gillies@colorado.edu>
2016-04-30 16:52:21 -0600

    Add password resets @ email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,16 @@ Rails.application.configure do
   config.force_ssl = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https'}
+  config.action_mailer.delivery_method = :smtp
+  host = 'sleepy-stream-16190.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
