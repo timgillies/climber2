239f968759765f1cac0b1589a2ff13efa0264bc0
Tim Gillies <timothy.gillies@colorado.edu>
2016-06-23 16:09:05 -0600

    updated second part of migration

--- db/migrate/20160515180013_add_user_id_to_walls.rb
+++ db/migrate/20160515180013_add_user_id_to_walls.rb
@@ -1,5 +1,6 @@
 class AddUserIdToWalls < ActiveRecord::Migration
   def change
     add_reference :walls, :user, index: true, foreign_key: true
+    add_reference :routes, :zone, index: true, foreign_key: true
   end
 end
