c518e96d78ed0ebed7098b81f9bc59631dcdf56d
Tim Gillies <timothy.gillies@colorado.edu>
2016-08-05 13:52:57 -0600

    updated email host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sleepy-stream-16190.herokuapp.com'
+  host = 'www.climbconnect.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
