63528914aef16b4c79ba4a4cc757afc49c23d0d6
Tim Gillies <timothy.gillies@colorado.edu>
2016-08-11 07:25:03 -0600

    fixed user route index

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -5,7 +5,6 @@ class RoutesController < ApplicationController
 
   def index
     @facility = Facility.find(params[:facility_id])
-    @tick = current_user.ticks.build
     @filterrific = initialize_filterrific(
       Route,
       params[:filterrific],
