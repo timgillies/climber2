b51579550b49bc0d229bcfe1fbf5b9424bb69247
Tim Gillies <timothy.gillies@colorado.edu>
2016-08-20 11:28:51 -0600

    updated homepage with marketing materials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
 200.times do |n|
-  name  = Faker::Name.title
+  name  = Faker::Book.title
   color = Faker::Color.color_name
-  grade_id = Faker::Number.between(151, 194)
-  setter_id = Faker::Number.between(1, 3)
-  zone_id = Faker::Number.between(26, 29)
-  wall_id = Faker::Number.between(24, 27)
-  Route.create!(user_id:  '2',
-               facility_id: '27',
+  grade_id = Faker::Number.between(18, 32)
+  setter_id = Faker::Number.between(2, 6)
+  zone_id = Faker::Number.between(7, 12)
+  wall_id = Faker::Number.between(2, 6)
+  start_date = Faker::Date.between(30.days.ago, Date.today)
+  end_date = Faker::Date.between(30.days.from_now, 60.days.from_now)
+  Route.create!(user_id:  '5',
+               facility_id: '3',
                name:              name,
                color:             color,
-               setdate: Date.today.strftime('%Y-%m-%d'),
-               enddate: 30.days.from_now.strftime('%Y-%m-%d'),
+               setdate:           start_date.strftime('%Y-%m-%d'),
+               enddate:           end_date.strftime('%Y-%m-%d'),
                grade_id:  grade_id,
                zone_id: zone_id,
                wall_id: wall_id,
