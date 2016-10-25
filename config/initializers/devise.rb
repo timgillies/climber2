f9c3200d0896acb0b3cbe4ec08d528008ec34c2d
Tim Gillies <timothy.gillies@colorado.edu>
2016-10-25 16:13:42 -0600

    update OAuth redirect URI

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -60,7 +60,7 @@ Devise.setup do |config|
   # config.params_authenticatable = true
 
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SEC'],
-                  callback_url: "http://www.climbconnect.com/users/auth/facebook/callback",
+                  callback_url: "https://climbconnect.com/users/auth/facebook/callback",
                   scope: 'email, public_profile',
                   info_fields: 'name,email,first_name,last_name,gender,location',
                   display: 'popup'
