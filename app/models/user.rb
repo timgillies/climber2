e46d5ebe3f999105873cebec9e95690722295e85
Tim Gillies <timothy.gillies@colorado.edu>
2016-10-26 14:35:28 -0600

    fixed email issue?

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,14 +21,6 @@ class User < ActiveRecord::Base
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
-  before_save   :downcase_email
-  # before_create :create_activation_digest
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255},
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6}, allow_nil: true
 
   ratyrate_rater
 
