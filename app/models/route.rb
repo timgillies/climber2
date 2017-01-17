532bd66558e27f21d23b9f6427ea20bdd054156e
Tim Gillies <timothy.gillies@colorado.edu>
2017-01-16 20:00:41 -0700

    fixed default filter for routes list for active issue

--- app/models/route.rb
+++ app/models/route.rb
@@ -67,7 +67,7 @@ class Route < ActiveRecord::Base
   ratyrate_rateable 'total'
 
   filterrific(
-  default_filter_params: { sorted_by: 'created_at_desc', with_status_id: Date.today },
+  default_filter_params: { sorted_by: 'created_at_desc', with_status_id: Date.current },
   available_filters: [
     :sorted_by,
     :search_query,
