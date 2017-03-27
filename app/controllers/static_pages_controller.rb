0fd11733cde262c80d1c2cb4a125550423e5f4c2
Tim Gillies <timothy.gillies@colorado.edu>
2017-03-27 08:44:32 -0600

    fixed first facility issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       if @userfacilities.count > 0
-        redirect_to admin_facility_path(@userfacilities.all)
+        redirect_to admin_facility_path(@userfacilities.first)
       end
     end
     @user = User.new
