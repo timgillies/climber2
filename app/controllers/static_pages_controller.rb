dc6f63f57e2c0bda83acb7b3c5e14faad55b9945
Tim Gillies <timothy.gillies@colorado.edu>
2017-03-27 08:58:51 -0600

    fixed redirect issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,7 @@ class StaticPagesController < ApplicationController
 
   def home
     if user_signed_in?
-      if @userfacilities.count > 0
-        redirect_to admin_facility_path(@userfacilities.first)
-      end
+
     end
     @user = User.new
 
