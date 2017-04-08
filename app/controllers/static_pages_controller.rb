7c62732a69770d4e63e080fc14e9b11edcd920d6
Tim Gillies <timothy.gillies@colorado.edu>
2017-04-08 13:38:58 -0600

    redirect users to inbox if they have a pending invite

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,11 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       if @userfacilities.count > 0
-        redirect_to admin_facility_path(@userfacilities.first)
+        if FacilityRole.where(email: current_user.email, user_id: nil).count > 0 #checks if user has any pending invites
+          redirect_to inbox_user_url(current_user) #redirects to root where they will be asked to confirm invite.
+        else
+          redirect_to admin_facility_path(@userfacilities.first)
+        end
       end
     end
     @user = User.new
