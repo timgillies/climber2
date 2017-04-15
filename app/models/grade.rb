47c3825ee74f34c4299de70cd60fccbea3ca69bd
Tim Gillies <timothy.gillies@colorado.edu>
2017-04-15 09:59:55 -0600

    updated depricated warning

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -3,7 +3,7 @@ class Grade < ActiveRecord::Base
   belongs_to :grade_system, :foreign_key => :grade_system_id
   has_many :routes
   has_many :grade_systems, :primary_key => :grade_system_id, :foreign_key => :id
-  has_many :facility_grade_systems, :through => :grade_systems
+  has_many :facility_grade_systems, {:through=>:grade_systems, :source=>:facility_grade_system}
   has_many :facilities, :through => :facility_grade_systems
   belongs_to :user
   has_many    :tasks
